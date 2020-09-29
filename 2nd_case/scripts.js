e2656fbe45431b2181beb205b780bda4b2a1930e
ALex Bezborodov <alexbezborodov23@gmail.com>
2020-09-29 21:12:29 +0300

    create ui design for db

---
+++ 2nd_case/scripts.js
@@ -0,0 +1,22 @@
+// const wrapper = document.createElement('div'),
+//       body = document.querySelector('body');
+
+// wrapper.style.cssText = 'min-width: 400px; width: 600px; height:auto; border: 2px solid black;border-radius: 20px; margin: 0 auto';
+// body.append(wrapper);
+
+// function createElement(width , height, line = 0, color = 'black', id) {
+
+//     const element = document.createElement('div');
+//     element.style.cssText = `width: ${width}px; height:${height}px; border-bottom: ${line}px solid ${color}; margin: 5px auto`;
+//     element.id = `${id};`
+//     wrapper.append(element);
+// }
+
+// createElement(580,80, 4, 'blue', 'title');
+// createElement(580,300, 2, 'grey', 'list');
+// createElement(580,70, 'input-holder');
+
+
+// let title = wrapper.querySelectorAll('div');
+//     title[0].innerHTML = '<h1>Data Base</h1>';
+//     title[0].style.cssText='text-align: center;color: red;';
