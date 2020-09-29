95642a93b9d06ef7e3da3f56f03b2b8da950c72c
ALex Bezborodov <alexbezborodov23@gmail.com>
2020-09-29 14:35:20 +0300

    create folder 1st_case

--- 1st_case/Scripts.js
+++ 1st_case/Scripts.js
@@ -20,13 +20,21 @@ function CreateBigCanvas() {
     mainCanvas.width = 600;
     mainCanvas.height = 600;
     mainCanvas.style.border = 'black 2px solid';
-    DrowCanvasElement(75,100,5,15,30,'red');
-    DrowCanvasElement(525,100,5,15,30,'blue');
+    DrowCanvasElement(75,100,5,15,30,'black');
+    DrowCanvasElement(525,100,5,15,30,'yellow');
     DrowCanvasElement(300,300,5,15,30,'green');
-    DrowCanvasElement(100,525,5,15,30,'yellow');
+    DrowCanvasElement(75,525,5,15,30,'yellow');
     DrowCanvasElement(525,525,5,15,30,'black');
+    DrowCanvasElement(175,100,5,15,30,'red');
+    DrowCanvasElement(425,100,5,15,30,'blue');
+    DrowCanvasElement(300,300,5,15,30,'green');
+    DrowCanvasElement(200,300,5,15,30,'grey');
+    DrowCanvasElement(400,300,5,15,30,'grey');
+    DrowCanvasElement(300,400,5,15,30,'grey');
+    DrowCanvasElement(300,200,5,15,30,'grey');
+    DrowCanvasElement(170,525,5,15,30,'blue');
+    DrowCanvasElement(425,525,5,15,30,'red');
             
-        
 }
 
 function CreateSmallCanvas() {
@@ -39,13 +47,14 @@ function CreateSmallCanvas() {
 
 function DrowCanvasElement(cx, cy, spikes, outerRadius, innerRadius, color) {
 
-    let rot = Math.PI/2*3,
+    let rot = Math.PI/2 * 3,
         x = cx,
         y = cy,
-        step = Math.PI/spikes;
+        step = Math.PI / 5;
 
         ct1.beginPath();
         ct1.moveTo(cx, cy - outerRadius);
+
         for (i = 0; i < spikes; i++) {
             x = cx + Math.cos(rot) * outerRadius;
             y = cy + Math.sin(rot) * outerRadius;
@@ -63,11 +72,7 @@ function DrowCanvasElement(cx, cy, spikes, outerRadius, innerRadius, color) {
         ct1.closePath();
         ct1.fillStyle = `${color}`;
         ct1.fill();
-    // ct1.fillStyle = 'red';
-    // ct1.fillRect(10, 10, 100, 100);
-    // ct1.fillStyle = 'blue';
-    // ct1.fillRect(300, 300, 190, 190);
-
+    
 }
 
 AddWrapper();
@@ -79,14 +84,13 @@ const getPixelColor = e => {
         pixel = ct1.getImageData(x, y, 1, 1);
         smallCanvasFill(pixel);
 
-
 };
-
-mainCanvas.addEventListener('click', getPixelColor);
 
 function smallCanvasFill(pixel) {
 
     smallCanvas.style.backgroundColor = `rgba(${pixel.data[0]}, ${pixel.data[1]}, ${pixel.data[2]}, ${pixel.data[3]})`;
     
 }
+
+mainCanvas.addEventListener('click', getPixelColor);
     
