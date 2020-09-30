45afb43c7d90e1be9b5fe1d8b3209199f3a75f79
ALex Bezborodov <alexbezborodov23@gmail.com>
2020-09-30 16:09:55 +0300

    create function to add items to db

--- 2nd_case/scripts.js
+++ 2nd_case/scripts.js
@@ -1,22 +1,54 @@
-// const wrapper = document.createElement('div'),
-//       body = document.querySelector('body');
-
-// wrapper.style.cssText = 'min-width: 400px; width: 600px; height:auto; border: 2px solid black;border-radius: 20px; margin: 0 auto';
-// body.append(wrapper);
-
-// function createElement(width , height, line = 0, color = 'black', id) {
-
-//     const element = document.createElement('div');
-//     element.style.cssText = `width: ${width}px; height:${height}px; border-bottom: ${line}px solid ${color}; margin: 5px auto`;
-//     element.id = `${id};`
-//     wrapper.append(element);
-// }
-
-// createElement(580,80, 4, 'blue', 'title');
-// createElement(580,300, 2, 'grey', 'list');
-// createElement(580,70, 'input-holder');
 
 
-// let title = wrapper.querySelectorAll('div');
-//     title[0].innerHTML = '<h1>Data Base</h1>';
-//     title[0].style.cssText='text-align: center;color: red;';
+const items = document.querySelector('.item'),
+      item = items.querySelector('.data-placeholder'),  
+      btnSave = document.querySelector("[name = 'btn-save']");
+      
+
+    let personData = [['Alex', +380955465591],['Karina',+380967597462],['Anatoliy',+380635425579],['Karina R', +380995427826]];      
+     
+    function showDB () {
+
+        items.innerHTML = '';
+        personData.forEach((data, i) => {
+            items.innerHTML += `
+            <div class="data-placeholder">
+                <div class="data-item">
+                    ${personData[i][0]}
+                </div>
+                <div class="data-item">
+                    ${personData[i][1]}            
+                </div>
+                <button class="btn-style">Del</button>
+            </div>    
+            `;
+        });
+    };
+    showDB();
+      
+
+
+
+
+
+const addItems = () => {
+    
+    const name = document.querySelector("[name = 'name']").value,
+          phone = document.querySelector("[name = 'phone']").value,
+          inputs = document.querySelectorAll('.input-data');
+          
+
+    if(name != ''&& name !=Number ) {
+        
+        personDataitem = [ name , Number(phone)];
+        personData.push(personDataitem);
+        console.log(personData);
+
+    }else {
+        console.log('Something wrong');
+    }
+    
+    showDB();
+    inputs.innerHTML = '';
+}
+btnSave.addEventListener('click', addItems);
